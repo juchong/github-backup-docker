d8bf8db5e786468321706ec9b82ee0941627c236
juchong <juan@plainmail.net>
2022-02-08 22:31:19 -0800

    Pushing changes to add unraid compatibility

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ FROM alpine:3.9
 RUN apk add --update --no-cache tzdata git python py-pip tzdata
 RUN pip install github-backup && github-backup -v
 COPY exec.sh /srv/exec.sh
+COPY config.json.example /srv/config/config.json.example
 RUN chmod +x /srv/exec.sh
 CMD ["/srv/exec.sh"]
--- exec.sh
+++ exec.sh
@@ -5,19 +5,33 @@ echo "timezone=${TIME_ZONE}"
 cp /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime
 echo "${TIME_ZONE}" >/etc/timezone
 
+# If config doesn't exist yet, create it
+if [ ! -f /srv/var/config/config.json ]; then
+    cp /srv/var/config/config.json.example /srv/var/config/config.json
+fi
+
+# Update config.json
+cp /srv/var/config/config.json /srv/var/config.json
+
+# Update token in config.json match $TOKEN environment variable
+sed -i '/token/c\   \"token\" : \"'${TOKEN}'\",' /srv/var/config.json
+
+# Return config.json to persistant volume
+cp /srv/var/config.json /srv/var/config/config.json
+
 echo "$(date) - start backup scheduler"
 while :; do
     DATE=$(date +%Y%m%d-%H%M%S)
 
     for u in $(echo $GITHUB_USER | tr "," "\n"); do
         echo "$(date) - execute backup for ${u}, ${DATE}"
-        github-backup ${u} --token=$TOKEN --all --output-directory=/srv/var/${DATE}/${u} --private --gists
+        github-backup ${u} --token=$TOKEN --output-directory=/srv/var/${DATE}/${u} ${OPTIONS}
     done
 
     echo "$(date) - cleanup"
 
     ls -d1 /srv/var/* | head -n -${MAX_BACKUPS} | xargs rm -rf
 
-    echo "$(date) - sleep for 1 day"
-    sleep 1d
+    echo "$(date) - sleep for ${slptime} day"
+    sleep ${slptime}
 done
